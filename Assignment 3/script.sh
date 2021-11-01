48a17944c8e76527a68ce6480b2743ecfd411234
Wei Zhong Tee <weizhong.tee@jamf.com>
2021-11-01 17:46:45 -0500

    Add script

---
+++ Assignment 3/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=GPU
+#SBATCH --time=5:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodelist=gpu02
+#SBATCH --mem=20G
+#SBATCH --job-name="Deep_Learning_Q2"
+#SBATCH --output=q2out.out
+#SBATCH --error=q2err.err
+#SBATCH --mail-user=teewz1076@UWEC.EDU
+#SBATCH --mail-type=END
+
+
+module load miniconda3
+
+python q2.py
